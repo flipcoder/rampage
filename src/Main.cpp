111e95874e472218f17c990ba4d9338d4593ef20
Grady O'Connell <flipcoder@gmail.com>
2014-11-17 12:37:43 -0800

    improved run(), listing rampages

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,6 +3,7 @@
 #include "kit/log/log.h"
 #include "kit/async/multiplexer.h"
 #include "kit/args/args.h"
+#include "kit/meta/meta.h"
 
 #include <sys/wait.h>
 #include <sys/types.h>
@@ -10,15 +11,20 @@
 
 using namespace std;
 
-//int run(string prog, vector<string> args)
-//{
-//    pid_t pid = fork();
-//    if(0 == pid)
-//        execl(prog, prog, // (char*)NULL);
-//    int status;
-//    waitpid(pid, &status, 0);
-//    return status;
-//}
+int run(string prog, vector<string> args)
+{
+    // make space for prog name and null
+    std::vector<char*> argsptrs(args.size()+2);
+    argsptrs[0] = (char*)prog.c_str();
+    for(size_t i=0;i!=args.size();++i)
+        argsptrs[i+1] = &args[i][0];
+    pid_t pid = fork();
+    if(0 == pid)
+        execvp(prog.c_str(), argsptrs.data());
+    int status;
+    waitpid(pid, &status, 0);
+    return status;
+}
 
 int main(int argc, const char *argv[])
 {
@@ -37,16 +43,14 @@ int main(int argc, const char *argv[])
                 break;
             if(boost::starts_with(dir, "-"))
                 continue;
-            pid_t pid = fork();
-            if(0 == pid)
-                execl(
-                    "/bin/diff", "diff", "-rupN",
-                    (string("/dev/shm/rpg/")+dir).c_str(),
-                    (string(".")+dir+".rpg").c_str(),
-                    (char*)NULL
-                );
-            int status;
-            waitpid(pid, &status, 0);
+            run(
+                "/bin/diff",
+                {
+                    "-rupN",
+                    string("/dev/shm/rpg/")+dir,
+                    string(".")+dir+".rpg"
+                }
+            );
         }
         return 1;
     }
@@ -59,21 +63,29 @@ int main(int argc, const char *argv[])
         return 1;
     }
     
-    for(unsigned i=0;;++i)
+    if(not args.empty()) for(unsigned i=0;;++i)
     {
         string dir = args.get(i);
         if(dir.empty())
             break;
 
-        pid_t pid = fork();
-        if(0 == pid)
-            execl(
-                "/bin/cp", "cp", "-r",
-                dir.c_str(), string("/dev/shm/rpg/").c_str(),
-                (char*)NULL
-            );
-        int status;
-        waitpid(pid, &status, 0);
+        int status = run(
+            "/bin/cp",
+            {
+                "-r",
+                dir,
+                "/dev/shm/rpg/"
+            }
+        );
+
+        //auto detail = make_shared<Meta>();
+        //detail->set<string>("path","");
+        //detail->serialize(string("/dev/shm/rpg/")+dir+".json");
+        
+        if(0 != status) {
+            cerr << "Cannot enrage. (copy failed)" << endl;
+            return 1;
+        }
         if(0 != rename(dir.c_str(), (string(".") + dir + ".rpg").c_str())) {
             cerr << "Cannot enrage. (rename failed)" << endl;
             unlink((string("/dev/shm/rpg/")+dir).c_str());
@@ -89,6 +101,12 @@ int main(int argc, const char *argv[])
         }
         cout << dir << " has went on a rampage!" << endl;
         return 0;
+    }
+    else
+    {
+        // no app parameters
+        cout << "Rampages: " << endl;
+        run("/bin/ls", {"/dev/shm/rpg/"});
     }
     
     return 0;
